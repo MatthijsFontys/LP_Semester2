5d3ca12ee3bcf0f753ea634e6eef020a72dfd9f5
Matthijs <matthijs.3mail@gmail.com>
2019-04-10 18:54:06 +0200

    Fixed bug that was still in the updating of the index pagina

--- MVC/MVC_ReleaseDateSite/wwwroot/js/UpdateFollowstateAjax.js
+++ MVC/MVC_ReleaseDateSite/wwwroot/js/UpdateFollowstateAjax.js
@@ -2,7 +2,6 @@ function UpdateFollowState(release, RefreshGuiFunction) {
     $.post("/release/" + release.dataset.followstate + "/" + release.dataset.releaseid, (data) => {
         let json = JSON.parse(data);
         release.dataset.followstate = json.followState;
-        console.log(release);
         RefreshGuiFunction(release, json.followCount, json.followState);
     });
 }
