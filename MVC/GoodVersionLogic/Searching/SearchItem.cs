e18d45d8652c98c514310cf438e314ac1675ecf3
Matthijs <matthijs.3mail@gmail.com>
2019-05-04 21:42:43 +0200

    Added read more to the single page

---
+++ MVC/GoodVersionLogic/Searching/SearchItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVC_ReleaseDateSite.Logic {
+    public class SearchItem {
+        public double TotalPoints { get; private set; } = 0;
+        public double CurrentWordPoints { get; private set; } = 0;
+        public int CurrentWordCount { get; private set; } = 0;
+        public IReadOnlyCollection<SearchZone> SearchZones { get; set; }
+
+        private void CurrentWorsPointsToTotal() {
+            TotalPoints += CurrentWordPoints;
+            CurrentWordPoints = 0;
+        }
+
+        private void AddPointsForCurrentWord(double points) {
+            CurrentWordPoints += points;
+        }
+
+        private void AddToWordCount() {
+            CurrentWordCount++;
+        }
+
+
+    }
+}
---
+++ MVC/GoodVersionLogic/Searching/SearchZone.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVC_ReleaseDateSite.Logic {
+    public class SearchZone {
+        public string Text { get; set; }
+        public char Splitter { get; set; }
+        public double Bias   { get; set; }
+    }
+}
