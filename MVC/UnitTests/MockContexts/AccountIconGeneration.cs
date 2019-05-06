9ed7953e5bff3f6df1b73691bae78dac7aee70c7
Matthijs <matthijs.3mail@gmail.com>
2019-05-06 12:01:08 +0200

    Added unit test for account icon generation

---
+++ MVC/UnitTests/MockContexts/AccountIconGeneration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using MVC_ReleaseDateSite.Logic;
+using MVC_ReleaseDateSite.Data;
+using MVC_ReleaseDateSite.Models;
+
+namespace UnitTests.MockContexts {
+    public class AccountIconGeneration {
+        AccountLogic accountLogic;
+        public AccountIconGeneration() {
+            accountLogic = new AccountLogic(
+                    new AccountRepository(
+                        new AccountMockContext()));
+        }
+
+        [Fact]
+        public void GenerateIconForNewAccount() {
+            // Assign
+            string expected = @"https://via.placeholder.com/50/000000/?text=A";
+            string actual = "";
+            User user = new User
+            {
+                Username = "Amano",
+                PasswordHash = "password" 
+            };
+            // Act
+           accountLogic.Add(user);
+           actual = actual + user.ImgLocation.Substring(0, 31) + user.ImgLocation.Substring(38);
+           // Assert
+           Assert.Equal(expected, actual);
+        }
+
+    }
+}
---
+++ MVC/UnitTests/MockContexts/AccountMockContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MVC_ReleaseDateSite.Data;
+using MVC_ReleaseDateSite.Interfaces;
+
+namespace UnitTests.MockContexts {
+    class AccountMockContext : IAccountContext {
+        public void Add(IUser type) {
+            Console.WriteLine("Added user");
+        }
+
+        public bool CheckLoginCredentials(IUser user) {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T2>(T2 primaryKey) {
+            throw new NotImplementedException();
+        }
+
+        public List<IUser> GetAll() {
+            throw new NotImplementedException();
+        }
+
+        public IUser GetByPrimaryKey<T2>(T2 id) {
+            throw new NotImplementedException();
+        }
+
+        public void Update(IUser type) {
+            throw new NotImplementedException();
+        }
+    }
+}
