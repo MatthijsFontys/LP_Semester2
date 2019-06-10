3d9f4e0d02b5ec1aeb30f5116cccf811d6eb103b
Matthijs <matthijs.3mail@gmail.com>
2019-06-10 12:52:28 +0200

    Finished final touches on the register

---
+++ MVC/MVC_ReleaseDateSite/wwwroot/js/RegisterAjax.js
@@ -0,0 +1,20 @@
+﻿let available;
+function IsUsernameAvailable(inputBox) {
+    let username = inputBox.value;
+    $.ajax({
+        type: "POST",
+        url: "/account/IsUsernameAvailable",
+        data: { "username": username }
+    }).done((data) => {
+        let json = JSON.parse(data);
+        let usernameValidationField = document.querySelector("#username-validation");
+        if (json.result == false && usernameValidationField.innerHTML == "") {
+            usernameValidationField.innerHTML += "This username already exists";
+            available = false;
+        }
+        else if (json.result == true && available == false) {
+            usernameValidationField.innerHTML = "";
+            available = true;
+        }
+    });
+}
